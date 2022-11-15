5637df6ec15e26ce908e716b96138171db955a7f
Eduardo Souza <103686965+eduardo074@users.noreply.github.com>
2022-11-14 21:50:06 -0300

    Att menu

--- js/script.js
+++ js/script.js
@@ -1,16 +1,19 @@
-window.onload = function () {
-    document.querySelector(".menuMobile").addEventListener("click", function () {
-        if(document.querySelector(".menu nav ul").style.display == 'flex'){
-            document.querySelector(".menu nav ul").style.display = 'none';
-           
-        } else {
-            document.querySelector(".menu nav ul").style.display = 'flex';
-            // document.querySelector("header").style.backgroundColor = '#2f2f2f';
-        }
-    });
-}
+
+document.querySelector(".menuMobile").addEventListener("click", function () {
+    if(document.querySelector(".menu nav ul").style.display == 'flex'){
+        document.querySelector(".menu nav ul").style.display = 'none';
+        
+    } else {
+        document.querySelector(".menu nav ul").style.display = 'flex';
+        // document.querySelector("header").style.backgroundColor = '#2f2f2f';
+    }
+});
+
 
 window.onload =  function loading() {
     document.getElementsByClassName('box-pre')[0].style.display = 'none';    
     document.getElementsByClassName('content')[0].style.display = 'block'; 
 }
+
+
+
